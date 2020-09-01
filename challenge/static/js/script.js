befc0e7c8e2b3a72b64f1b82481447799f9aa322
mahmudibit23 <41866245+mahmudibit23@users.noreply.github.com>
2020-09-01 16:55:39 +0600

    Add files via upload
    challenge -2 complete

---
+++ challenge/static/js/script.js
@@ -0,0 +1,25 @@
+// Challenge 1 your age in days. 
+
+function ageInDays(){
+var birthYear = prompt('What year were you born.. good friend');
+var ageInDayss = (2020 - birthYear) * 365;
+console.log(ageInDayss);
+var h1 = document.createElement('h1');
+var textAnswer = document.createTextNode('Your are ' + ageInDayss + ' days');
+h1.setAttribute('id','ageInDays');
+h1.appendChild(textAnswer);
+document.getElementById('flex-box-result').appendChild(h1);
+}
+
+function reset(){
+    document.getElementById('ageInDays').remove();
+}
+
+//challenge -2 get cat
+function generateCat(){
+    var image = document.createElement('img');
+    var div = document.getElementById('flex-cat-gen');
+    image.src = "http://thecatapi.com/api/images/get?format=src&type=jpg&size=small&"
+    div.appendChild(image);
+
+}
